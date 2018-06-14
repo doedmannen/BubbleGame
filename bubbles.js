7e2724d8711b35f4171ea8bbac7686e0a8fb66e4
Kristoffer Katra <doedmannen@aetmej.se>
2018-06-14 12:43:45 +0200

    New location for movement and spawn has been moved to location.js

--- bubbles.js
+++ bubbles.js
@@ -1,31 +1,23 @@
-/*
-newSpawn is used to make sure that the bubbles don't spawn to close to the
-players x and y location. This is to prevent player from getting game over at
-random or picking up green bubbles without moving.
-The function is recursive and returns an array containing suitable x and y.
-*/
-var newSpawn = function(){
-  let spawnX = random(wall, width-wall*2);
-  let spawnY = random(wall, height-wall*2);
-  let d = dist(spawnX, spawnY, ourHero.x, ourHero.y);
-  if(d > 50 + 20 + ourHero.r){
-    return [spawnX, spawnY];
-  }
-  return newSpawn();
-}
-
-
 class bubble{
 
   constructor(type="RED"){
-    spawn = newSpawn();
-    this.x = spawn[0];
-    this.y = spawn[1];
+    fill(255,100,0);
+
+    setBubble = checkPos.spawn();
+    this.x = setBubble[0];
+    this.y = setBubble[1];
     this.type = type;         // What kind of bubble is it?
-    this.r = 20;              // Yellow bubbles should always be the larger size
-    if(type != "YELLOW"){
-      this.r = random(5,20);  // Green and red gets random size
+    if(type == "YELLOW"){
+      this.r = 20;  // Green and red gets random size
+    } else if(type == "GREEN"){
+      this.r = random(15, 20);
+    } else {
+      this.r = random(8, 13);
     }
+    setBubble = checkPos.add();
+    this.xN = setBubble[0];
+    this.yN = setBubble[1];
+
   }
 
   // Function that checks if a bubble hits the player
@@ -42,43 +34,63 @@ class bubble{
     if(this.type == "GREEN"){
       fill(0, 255, 0, 150);
     }else if(this.type == "YELLOW"){
-      fill(255, 255, 0, 150)
+      fill(255, 255, 0, 150);
+      if(counter > 400 && counter % 5 == 0){
+        fill(0);
+      }
     }
     ellipse(this.x, this.y, this.r*2);
+    if(this.type != "RED"){
+      fill(0);
+      ellipse(this.x, this.y, this.r);
+      if(this.type == "YELLOW"){
+        fill(255);
+        ellipse(this.x, this.y, 4);
+      }
+    }
   }
 
 
   // Function to move the bubbles
   // Position is checked against outer walls of gameboard
   move(){
+
+    if(counter == 1){
+      checkPos.delete(this.x, this.y);
+    }
+
+    // If all the greens are picked, move all bubbles outside of board
     if(pickUps < 1){
       this.x = -100;
     }
 
+    /*
+    Yellow bubbles should only exist for a limited time. Once the timer
+    runs out, the bubble is moved out of the gameboard.
+    */
+    if(this.type == "YELLOW" && counter == countTimer){
+      this.x = -100;
+    }
+
     if(this.x > 0){ // Is the object active?
-      // Checks the position to make sure object stays inside the walls
-      if(this.x-this.r-1 < wall){
-        this.x = this.x + random(0, 2);
-      } else if(this.x+this.r+1 > width-wall){
-        this.x = this.x + random(-2, 0);
-      } else {
-        this.x = this.x + random(-2, 2);
+
+      if((this.x > this.xN - moveSpeed && this.x < this.xN + moveSpeed) ||
+          (this.y > this.yN - moveSpeed && this.y < this.yN + moveSpeed)){
+            checkPos.delete(this.xN, this.yN);
+            setBubble = checkPos.add();
+            this.xN = setBubble[0];
+            this.yN = setBubble[1];
       }
 
-      if(this.y-this.r-1 < wall){
-        this.y = this.y + random(0, 2);
-      } else if(this.y+this.r+1 > height-wall){
-        this.y = this.y + random(-2, 0);
-      } else {
-        this.y = this.y + random(-2, 2);
+      if(this.x < this.xN){
+        this.x += moveSpeed;
+      } else if (this.x > this.xN){
+        this.x -= moveSpeed;
       }
-
-      /*
-      Yellow bubbles should only exist for a limited time. Once the timer
-      runs out, the bubble is moved out of the gameboard.
-      */
-      if(this.type == "YELLOW" && counter == countTimer){
-        this.x = -100;
+      if(this.y < this.yN){
+        this.y += moveSpeed;
+      } else if(this.y > this.yN){
+        this.y -= moveSpeed;
       }
     }
 
@@ -100,18 +112,31 @@ class bubble{
 
       } else if(this.type == "YELLOW"){
 
-          ourHero.r -= 5;     // Shrink player in case of yellow
-          if(ourHero.r < 10){
-            ourHero.r = 10;   // Players size should be above 10
+        ourHero.r = 10; // Reset size of player
+        
+        score += 3;              // Increase score
+        if(score > highscore){  // Update highscore
+          highscore = score;
+        }
 
-          }
+
+
       } else {
-                              // The pickup was green
-        ourHero.r += 0.2;     // Increase players size
-        pickUps--;            // Decrease pickups
+                                // The pickup was green
+        ourHero.r += 0.2;       // Increase players size
+        pickUps--;              // Decrease pickups
+        score++;                // Increase score
+        if(score > highscore){  // Update highscore
+          highscore = score;
+        }
         if(pickUps < 1){
           counter = 0;
           level++;
+          if(level % 10 == 0){
+            moveSpeed += 0.2;
+            redBubbles = 0;
+            greenBubbles = 0;
+          }
         }
       }
     }
